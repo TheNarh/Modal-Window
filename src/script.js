af7ffb24bcb04776ef5b2e0cd943a40bec675f91
Ludwig <41292435+TheNarh@users.noreply.github.com>
2025-07-08 16:44:05 +0000

    Make modal functional by manipulating classes by removing and adding them using JavaScript.

--- src/script.js
+++ src/script.js
@@ -5,7 +5,26 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
+// function to open modal
+const openModal = function() {
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+}
+// function to close modal
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
 
 for(let i = 0; i < btnsOpenModal.length; i++) {
-    console.log(btnsOpenModal[i].textContent);
+    btnsOpenModal[i].addEventListener('click', openModal)
 }
+
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+            closeModal();
+    }
+})
